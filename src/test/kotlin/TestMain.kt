5520b465ebb27b949ee752a68c46cf3ffeb2c32e
Displee <displee@hotmail.com>
2020-05-30 05:34:08 +0200

    Remove TODO.

--- src/test/kotlin/TestMain.kt
+++ src/test/kotlin/TestMain.kt
@@ -2,7 +2,6 @@ import com.displee.undertow.server.UndertowVH
 import com.displee.web.localhost.LocalHost
 
 fun main() {
-    //TODO Add extends and include tests
     val server = UndertowVH("0.0.0.0", 80)
     server.register(LocalHost())
     server.start()
